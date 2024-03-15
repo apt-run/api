9658c9df812d81a6d910a2a68a9b24a09b58f58b
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-15 16:00:33 -0700

    Update models.go

--- database/models.go
+++ database/models.go
@@ -18,25 +18,6 @@ type PackageStats struct {
 	Maintainer string
 }
 
-// https://sources.debian.org/api/list/
-type List struct {
-	Packages []struct {
-		Name string `json:"name"`
-	} `json:"packages"`
-}
-
-// https://sources.debian.org/api/search/
-type SearchResults struct {
-	Query   string `json:"query"`
-	Results struct {
-		Exact interface{} `json:"exact"`
-		Other []struct {
-			Name string `json:"name"`
-		} `json:"other"`
-	} `json:"results"`
-	Suite string `json:"suite"`
-}
-
 // https://sources.debian.org/api/src/package_name/package_version
 type Package struct {
 	Package  string     `json:"package"`
@@ -49,30 +30,6 @@ type Package struct {
 		Suites  []string `json:"suites"`
 		Version string   `json:"version"`
 	} `json:"versions"`
-}
-
-func (data List) Value() (driver.Value, error) {
-	return json.Marshal(data)
-}
-
-func (list *List) Scan(value interface{}) error {
-	encoded_json, ok := value.([]byte)
-	if !ok {
-		return errors.New("type assertion to []byte failed")
-	}
-	return json.Unmarshal(encoded_json, &list)
-}
-
-func (data SearchResults) Value() (driver.Value, error) {
-	return json.Marshal(data)
-}
-
-func (data *SearchResults) Scan(value interface{}) error {
-	encoded_json, ok := value.([]byte)
-	if !ok {
-		return errors.New("type assertion to []byte failed")
-	}
-	return json.Unmarshal(encoded_json, &data)
 }
 
 func (data Package) Value() (driver.Value, error) {
